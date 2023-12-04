bf3c50ce7d957f4369db09c3175adfe9ae716adb
EiichiMiyagawa <m.monobyte@gmail.com>
2023-12-04 16:26:46 +0900

    Revert "修正タスク対応"
    This reverts commit 846020a42a206323eeeb4deb85d9fd4addeee991.

--- update.js
+++ update.js
@@ -1,21 +1,9 @@
 import { CSV } from "https://js.sabae.cc/CSV.js";
 import { Day, TimeZone } from "https://js.sabae.cc/DateTime.js";
 
-const domain = Deno.args[0];
-const getLatestHotelApi = domain + Deno.args[1];
-const getLatestRsvSumApi = domain + Deno.args[2];
-const getLatestRsvPrefectureSumApi = domain + Deno.args[3];
+const url = Deno.args[0];
 
-const hotelData = await (await fetch(getLatestHotelApi)).json();
-await Deno.writeTextFile("latest_hotel.csv", CSV.stringify(hotelData));
+const data = await (await fetch(url)).json();
+await Deno.writeTextFile("data/" + new Day(TimeZone.JST).toString() + ".csv", CSV.stringify(data));
+await Deno.writeTextFile("latest_rsv_sum.csv", CSV.stringify(data));
 
-const latestRsvSumData = await (await fetch(getLatestRsvSumApi)).json();
-await Deno.writeTextFile("latest_rsv_sum.csv", CSV.stringify(latestRsvSumData));
-
-const backupLatestRsvSumData = latestRsvSumData.filter(d => {
-    return d.date_visit >= new Day(TimeZone.JST).toString()
-});
-await Deno.writeTextFile("data/" + new Day(TimeZone.JST).toString() + ".csv", CSV.stringify(backupLatestRsvSumData));
-
-const latestRsvPrefectureSumData = await (await fetch(getLatestRsvPrefectureSumApi)).json();
-await Deno.writeTextFile("latest_rsv_prefecture_sum.csv", CSV.stringify(latestRsvPrefectureSumData));
